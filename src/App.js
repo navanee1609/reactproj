6951e0942543117f5ef139a68911858e3f025a1e
navanee1609 <navaneethanvs18@gmail.com>
2023-12-13 12:09:11 +0530

    added required files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     react practice solutions
+    </div>
+  );
+}
+
+export default App;
